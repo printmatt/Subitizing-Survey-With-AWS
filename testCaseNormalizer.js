87d8f12550eb68702e1b61e35b7ef92b1056f278
Matt Vang <50967496+printmatt@users.noreply.github.com>
2022-04-04 21:53:22 -0400

    script to fix bad test file

---
+++ testCaseNormalizer.js
@@ -0,0 +1,21 @@
+const data = require('./test(3).json');
+const fs = require("fs");
+
+data.testCases = data.testCases.map(test => {
+    test.dotPatterns = test.dotPatterns.map(pattern => {
+        pattern.xCoord = pattern.xCoord/440;
+        pattern.yCoord = pattern.yCoord/400;
+        return pattern
+    })
+    return test
+})
+
+console.log(data.testCases);
+
+fs.writeFile("denormalizedTest.json", JSON.stringify(data), err => {
+     
+    // Checking for errors
+    if (err) throw err; 
+   
+    console.log("Done writing"); // Success
+});
