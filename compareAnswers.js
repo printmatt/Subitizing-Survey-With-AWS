ac41dd27e40867bbbf1a039f62bb47a17cd7e32b
Matt Vang <50967496+printmatt@users.noreply.github.com>
2022-05-28 11:11:48 -0700

    results from sample run

---
+++ compareAnswers.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+const { parse } = require('csv-parse');
+const { stringify } = require('csv-stringify');
+var data = null
+
+// parse in data and start processing
+var parser = parse({ columns: true }, function (err, records) {
+    data = records;
+
+    for (let i = 0; i < data.length; i++) {
+        var userAns = JSON.parse(data[i].answers)
+        console.log(data[i].version)
+        data[i].correctAns = []
+        data[i].correctAns = compareAns(userAns, data[i].version)
+
+    }
+
+    fs.writeFileSync('results.json',JSON.stringify(data));
+
+
+});
+
+fs.createReadStream('subitizingResult.csv').pipe(parser);
+
+
+
+
+const compareAns = (userAns, version) => {
+    var correct = []
+    jsonData = JSON.parse(fs.readFileSync("./public/Version" + version + ".json", "utf8"))
+
+    for (let i = 0; i < jsonData.testCases.length; i++) {
+        correct[i] = jsonData.testCases[i].dotPatterns.length == userAns[i].S ? 1 : 0;
+    }
+
+    return correct;
+
+
+}
