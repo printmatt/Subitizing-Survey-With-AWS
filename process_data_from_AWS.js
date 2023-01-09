28c968af90bbc47cd692c924ce64b38e2d6b7f89
Matt Vang <50967496+printmatt@users.noreply.github.com>
2023-01-09 18:20:08 -0500

    first round AWS data. processed to check correctness.

--- process_data_from_AWS.js
+++ process_data_from_AWS.js
@@ -17,11 +17,22 @@ var parser = parse({ columns: true }, function (err, records) {
         data[i].answers = userAns.map((ans) =>{
             return parseInt(ans["S"]);
         })
-
+        var score = 0
+        var dropped = false
+        data[i].correctAns.forEach((correct) => {
+            if (correct == 1){
+                score += 1;
+            }
+            if (correct == -1){
+                dropped = true
+            }
+        })
+        data[i].score = score
+        data[i].dropped = dropped ? "yes" : ""
     }
 
-    fs.writeFileSync('AWSresults.json',JSON.stringify(data));
-    new objectsToCsv(data).toDisk('./AWSresults.csv');
+    fs.writeFileSync('./AWSResults/AWSResults.json',JSON.stringify(data));
+    new objectsToCsv(data).toDisk('./AWSResults/AWSResults.csv');
 
 
 });
@@ -36,10 +47,15 @@ fs.createReadStream('subitizingResult.csv').pipe(parser);
 const compareAns = (userAns, version) => {
     var correct = []
     jsonData = JSON.parse(fs.readFileSync("./public/Version" + version + ".json", "utf8"))
-
-    for (let i = 0; i < jsonData.testCases.length; i++) {
-        correct[i] = jsonData.testCases[i].dotPatterns.length == userAns[i].S ? 1 : 0;
+    console.log(userAns.length)
+    for (let i = 0; i < userAns.length; i++) {
+        correct[i] = jsonData.testCases[i].dotPatterns.length == userAns[i]["S"] ? 1 : 0;
     }
+
+    for (let i = userAns.length; i < jsonData.testCases.length; i++) {
+        correct[i] = -1;
+    }
+
 
     return correct;
 
